aca48c7a6994319a23bce9441b808cf51004df65
pla1n <danilkuisis@gmail.com>
2025-09-02 20:13:21 +0300

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
-import HomePage from "pages/HomePage/HomePage";
-import MoviesPage from "pages/Movies/MoviesPage";
-import { Route, Routes } from "react-router-dom";
+import HomePage from 'pages/HomePage/HomePage';
+import MoviesPage from 'pages/Movies/MoviesPage';
+import { Route, Routes } from 'react-router-dom';
+import Nav from './Nav/Nav';
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/movies" element={<MoviesPage/>}/>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Route>
       </Routes>
     </div>
   );
