a7d256e701d2b35b5543f8c3f42eddc33d6c7e57
pla1n <danilkuisis@gmail.com>
2025-09-02 19:59:34 +0300

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import HomePage from "pages/HomePage/HomePage";
+import MoviesPage from "pages/Movies/MoviesPage";
+import { Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/movies" element={<MoviesPage/>}/>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <>
+        <header>
+            <nav>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/movies'>Movies</NavLink>
+            </nav>
+        </header>
+        <main>
+            <Outlet/>
+        </main>
+        </>
+    )
+}
+
+export default Nav;
